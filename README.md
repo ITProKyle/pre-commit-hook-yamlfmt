74e09abce01869073890f06bff9144a1442e5395
Kyle Finley <kyle@finley.sh>
2022-01-18 13:24:43 -0500

    update to use poetry, pyright, black, isort

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 YAML formatter for [pre-commit](http://pre-commit.com).
 
-This hook formats the indentation of YAML files and
-optionally aligns top-level colons.<br/>
-It uses [ruamel.yaml](https://yaml.readthedocs.io/en/latest/)
-to do the heavy lifting and preserve comments within YAML files.
+This hook formats the indentation of YAML files and optionally aligns top-level colons.
+
+It uses [ruamel.yaml](https://yaml.readthedocs.io/en/latest/) to do the heavy lifting and preserve comments within YAML files.
 
 <!--TOC-->
 
@@ -31,10 +30,12 @@ to do the heavy lifting and preserve comments within YAML files.
 
 Add to `.pre-commit-config.yaml` in your git repo:
 
-    - repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
-      rev: 0.0.11  # or specific tag
-      hooks:
-          - id: yamlfmt
+```yaml
+- repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
+  rev: 0.0.11  # or specific tag
+  hooks:
+      - id: yamlfmt
+```
 
 :bulb: If a pre-commit hook changes a file,
 the hook fails with a warning that files were changed.
@@ -49,47 +50,52 @@ The default settings are:
 
 Given this input:
 
-    foo:
-      bar:
-        - baz1
-        - baz2
+```yaml
+foo:
+  bar:
+    - baz1
+    - baz2
+```
 
 The default settings result in this output:
 
-    ---
-    foo:
-        bar:
-            - baz1
-            - baz2
-
+```yaml
+---
+foo:
+    bar:
+        - baz1
+        - baz2
+```
 
 #### Combine with `yamllint`
 
 `yamlfmt` only works with valid YAML files, so
 I recommend to use `yamllint` and `yamlfmt` together.
 
-    - repo: https://github.com/adrienverge/yamllint.git
-      rev: v1.21.0  # or higher tag
-      hooks:
-          - id: yamllint
-            args: [--format, parsable, --strict]
+```yaml
+- repo: https://github.com/adrienverge/yamllint.git
+  rev: v1.21.0  # or higher tag
+  hooks:
+      - id: yamllint
+        args: [--format, parsable, --strict]
 
-    - repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
-      rev: 0.0.11  # or specific tag
-      hooks:
-          - id: yamlfmt
-
+- repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
+  rev: 0.0.11  # or specific tag
+  hooks:
+      - id: yamlfmt
+```
 
 #### Override defaults
 
 Add to `.pre-commit-config.yaml` in your git repo:
 
-    - repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
-      rev: 0.0.11  # or specific tag
-      hooks:
-          - id: yamlfmt
-            args: [--mapping, '2', --sequence, '2', --offset, '0', --colons, --width, '150']
-
+```yaml
+- repo: https://github.com/jumanjihouse/pre-commit-hook-yamlfmt
+  rev: 0.0.11  # or specific tag
+  hooks:
+      - id: yamlfmt
+        args: [--mapping, '2', --sequence, '2', --offset, '0', --colons, --width, '150']
+```
 
 ### Invoke pre-commit
 
@@ -97,29 +103,29 @@ Add to `.pre-commit-config.yaml` in your git repo:
 
 If you want to invoke the checks as a git pre-commit hook, run:
 
-    # Run on every commit.
-    pre-commit install
-
+```console
+# Run on every commit.
+pre-commit install
+```
 
 #### On demand
 
 If you want to run the checks on-demand (outside of git hooks), run:
 
-    # Run on-demand.
-    pre-commit run --all-files
+```console
+# Run on-demand.
+pre-commit run --all-files
+```
 
 The [test harness](TESTING.md) of this git repo uses this approach.
-
 
 ## Contributing
 
 Please see [CONTRIBUTING.md](CONTRIBUTING.md).
 
-
 ## Testing
 
 Please see [TESTING.md](TESTING.md).
-
 
 ## License
 
---
+++ pre_commit_hooks/yamlfmt.py
@@ -0,0 +1,188 @@
+#! /usr/bin/env python3
+"""Format YAML files."""
+from __future__ import annotations
+
+import argparse
+import locale
+import sys
+from dataclasses import asdict, dataclass, field
+from functools import cached_property
+from typing import TYPE_CHECKING, Any, List, Literal, NoReturn
+
+from ruamel.yaml import YAML
+
+if TYPE_CHECKING:
+    from _typeshed import StrPath
+
+
+DEFAULT_COLONS = False
+DEFAULT_IMPLICIT_START = True
+DEFAULT_INDENT_MAPPING = 4
+DEFAULT_INDENT_OFFSET = 4
+DEFAULT_INDENT_SEQUENCE = 6
+DEFAULT_PRESERVE_QUOTES = False
+DEFAULT_WIDTH = 150
+
+
+@dataclass
+class Args:
+    """Hook arguments."""
+
+    colons: bool = DEFAULT_COLONS
+    file_names: List[str] = field(default_factory=list)
+    implicit_start: bool = DEFAULT_IMPLICIT_START
+    mapping: int = DEFAULT_INDENT_MAPPING
+    offset: int = DEFAULT_INDENT_OFFSET
+    preserve_quotes: bool = DEFAULT_PRESERVE_QUOTES
+    sequence: int = DEFAULT_INDENT_SEQUENCE
+    width: int = DEFAULT_WIDTH
+
+
+def bool_action(default: bool) -> Literal["store_false", "store_true"]:
+    """Argparse bool action based on default value."""
+    return "store_false" if default else "store_true"
+
+
+class Cli:
+    """Command line interface."""
+
+    def __init__(self) -> None:
+        """Instantiate class."""
+        parser = argparse.ArgumentParser(
+            description="Format YAML files",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+            epilog="Tips at https://yaml.readthedocs.io/en/latest/detail.html",
+        )
+        parser.add_argument(
+            "-m",
+            "--mapping",
+            type=int,
+            default=DEFAULT_INDENT_MAPPING,
+            help="number of spaces to indent mappings (dictionaries)",
+        )
+        parser.add_argument(
+            "-s",
+            "--sequence",
+            type=int,
+            default=DEFAULT_INDENT_SEQUENCE,
+            help="number of spaces to indent sequences (arrays/lists)",
+        )
+        parser.add_argument(
+            "-o",
+            "--offset",
+            type=int,
+            default=DEFAULT_INDENT_OFFSET,
+            help="number of spaces to offset the dash from sequences",
+        )
+        parser.add_argument(
+            "-c",
+            "--colons",
+            action=bool_action(DEFAULT_COLONS),
+            help="whether to align top-level colons",
+        )
+        parser.add_argument(
+            "-w",
+            "--width",
+            type=int,
+            default=DEFAULT_WIDTH,
+            help="maximum line width",
+        )
+        parser.add_argument(
+            "-p",
+            "--preserve-quotes",
+            action=bool_action(DEFAULT_PRESERVE_QUOTES),
+            help="whether to keep existing string quoting",
+        )
+        parser.add_argument(
+            "-e",
+            "--implicit_start",
+            action=bool_action(DEFAULT_IMPLICIT_START),
+            help="whether to remove the explicit document start",
+        )
+        parser.add_argument(
+            "file_names",
+            metavar="FILE_NAME",
+            nargs="*",
+            help="space-separated list of YAML file names",
+        )
+        self.parser = parser
+
+    @cached_property
+    def args(self) -> Args:
+        """CLI arguments."""
+        return Args(**self.parser.parse_args().__dict__)
+
+
+class Formatter:
+    """Reformat a yaml file with proper indentation. Preserve comments."""
+
+    def __init__(
+        self,
+        colons: bool = DEFAULT_COLONS,
+        implicit_start: bool = DEFAULT_IMPLICIT_START,
+        mapping: int = DEFAULT_INDENT_MAPPING,
+        offset: int = DEFAULT_INDENT_OFFSET,
+        preserve_quotes: bool = DEFAULT_PRESERVE_QUOTES,
+        sequence: int = DEFAULT_INDENT_SEQUENCE,
+        width: int = DEFAULT_WIDTH,
+        **__kwargs: Any,
+    ) -> None:
+        """Instantiate class."""
+        yaml = YAML()
+        yaml.indent(
+            mapping=mapping,
+            sequence=sequence,
+            offset=offset,
+        )
+        yaml.top_level_colon_align = colons
+        yaml.explicit_start = implicit_start
+        yaml.width = width
+        yaml.preserve_quotes = preserve_quotes
+
+        self.yaml = yaml
+        self.content = list({})
+
+    def format(self, path: StrPath) -> None:
+        """Read file and write it out to same path."""
+        print(path, end="")
+        self.parse_file(path)
+        self.write_file(path)
+        print("  Done")
+
+    def parse_file(self, path: StrPath) -> None:
+        """Read the file."""
+        try:
+            with open(
+                path, "r", encoding=locale.getpreferredencoding(do_setlocale=False)
+            ) as stream:
+                self.content = list(self.yaml.load_all(stream))  # type: ignore
+        except IOError:
+            self.fail(f"Unable to read {path}")
+
+    def write_file(self, path: StrPath) -> None:
+        """Write the file."""
+        try:
+            with open(
+                path, "w", encoding=locale.getpreferredencoding(do_setlocale=False)
+            ) as stream:
+                self.yaml.dump_all(self.content, stream)
+        except IOError:
+            self.fail(f"Unable to write {path}")
+
+    @staticmethod
+    def fail(msg: str) -> NoReturn:
+        """Abort."""
+        sys.stderr.write(msg)
+        sys.exit(1)
+
+
+def main() -> None:
+    """Execute module."""
+    cli = Cli()
+    formatter = Formatter(**asdict(cli.args))
+    for file_name in cli.args.file_names:
+        formatter.format(file_name)
+
+
+if __name__ == "__main__":
+    main()
